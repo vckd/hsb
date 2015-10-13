1f12e80d3521aaa6e2003b98752ed4492f8fc871
chal <venuraalwis@gmail.com>
2015-10-13 11:15:18 +0530

    Jooq integration - postgres setting updated

--- src/main/java/sample/jooq/JooqConfiguration.java
+++ src/main/java/sample/jooq/JooqConfiguration.java
@@ -1,6 +1,7 @@
 package sample.jooq;
 
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
@@ -14,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 @AutoConfigureAfter(DataSourceAutoConfiguration.class)
@@ -23,12 +26,34 @@ public class JooqConfiguration {
     private Environment environment;
 
     @Bean
+    public DataSource dataSource() throws URISyntaxException {
+
+        URI dbUrl = new URI(System.getenv("DATABASE_URL"));
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(getDatabaseUrl(dbUrl));
+        dataSource.setUsername(getUsername(dbUrl));
+        dataSource.setPassword(getPassword(dbUrl));
+        dataSource.setMinEvictableIdleTimeMillis(30L * 60L * 1000L);
+        dataSource.setTimeBetweenEvictionRunsMillis(30L * 60L * 1000L);
+        dataSource.setNumTestsPerEvictionRun(3);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(false);
+        dataSource.setTestOnReturn(false);
+        dataSource.setValidationQuery("SELECT 1");
+        return dataSource;
+    }
+
+    @Bean
     public DataSourceConnectionProvider dataSourceConnectionProvider(DataSource dataSource) {
         return new DataSourceConnectionProvider(dataSource);
     }
 
     @Bean
     public DefaultConfiguration defaultConfiguration(DataSource dataSource) {
+        System.out.println("*************************here we  go****************");
+        System.out.println(System.getenv("PISSU_PUSA"));
+        System.out.println("*************************here we end****************");
         DefaultConfiguration defaultConfiguration = new DefaultConfiguration();
         String sqlDialect = environment.getRequiredProperty("spring.jooq.sqlDialect");
         SQLDialect dialect = SQLDialect.valueOf(sqlDialect);
@@ -46,6 +71,26 @@ public class JooqConfiguration {
     @Bean
     public JooqExceptionTranslator jooqExceptionTranslator(){
         return new JooqExceptionTranslator();
+    }
+
+    private String getDatabaseUrl(URI dbUrl) {
+        StringBuilder sb = new StringBuilder("jdbc:postgresql://")
+                .append(dbUrl.getHost())
+                .append(dbUrl.getPath());
+        String query = dbUrl.getQuery();
+        if (query != null) {
+            sb.append("?")
+                    .append(dbUrl.getQuery());
+        }
+        return sb.toString();
+    }
+
+    private String getUsername(URI dbUrl) {
+        return dbUrl.getUserInfo().split(":")[0];
+    }
+
+    private String getPassword(URI dbUrl) {
+        return dbUrl.getUserInfo().split(":")[1];
     }
 
 }
